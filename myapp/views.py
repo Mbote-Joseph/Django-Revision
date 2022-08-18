efb2b570a6eebd903767a67a9c03da4b34dbd637
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-18 23:32:58 +0300

    Django Revission

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django.shortcuts import render
 # from django.generics import ListView, DetailView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
@@ -17,53 +16,3 @@ def index(request):
     form = CommentForm
     total=posts.count()
     return render(request, 'index.html', {'name': name, 'form': form, 'posts': posts, 'total': total})
-
-class PostList(ListView):
-    model = Post.objects.all()
-    template_name = 'myapp/index.html'
-    context_object_name = 'posts'
-    paginate_by = 5
-    ordering = ['-created_at']
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = PostForm()
-        return context
-
-class CommentList(ListView):
-    model = Comment
-    template_name = 'myapp/comment.html'
-    context_object_name = 'comments'
-    paginate_by = 5
-    ordering = ['-created_at'] 
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = CommentForm()
-        return context
-
-
-class PostDetail(DetailView):
-    model = Post.objects.all()
-    template_name = 'myapp/detail.html'
-    context_object_name = 'post'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = CommentForm()
-        return context
-
-
-class CommentDetail(DetailView):
-    model = Comment
-    template_name = 'myapp/comment_detail.html'
-    context_object_name = 'comment'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = CommentForm()
-        return context 
-
-
-
-
